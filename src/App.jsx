9b582c81abdb5f50750233815ed48fed76e6c79c
Andrew <zakhary0000@gmail.com>
2023-05-28 18:29:34 -0400

    Completed login, results, matching, some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,229 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [breeds, setBreeds] = useState([]);
+  const [dogs, setDogs] = useState([]);
+  const [selectedDogs, setSelectedDogs] = useState([]);
+  const [selectedBreed, setSelectedBreed] = useState('');
+  const [zipCodes, setZipCodes] = useState('');
+  const [ageMin, setAgeMin] = useState('');
+  const [ageMax, setAgeMax] = useState('');
+  const [size, setSize] = useState(25);
+  const [sort, setSort] = useState('asc');
+  const [match, setMatch] = useState(null);
+  const [sortField, setSortField] = useState("");
 
+  function handleSelectDog(dog) {
+    setSelectedDogs(prevDogs => [...prevDogs, dog]);
+  }
+
+  function login() {
+    fetch("https://frontend-take-home-service.fetch.com/auth/login", {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({"name": name, "email": email}),
+      credentials: 'include'
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        return response.text();
+      })
+      .then((data) => {
+        console.log("Login response: ", data);
+        setIsLoggedIn(true)
+        getBreeds();
+      })
+      .catch(error => console.error("Login error: ", error));
+  }
+
+  function getBreeds() {
+    fetch('https://frontend-take-home-service.fetch.com/dogs/breeds', {
+      credentials: 'include',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Breeds: ", data);
+        setBreeds(data);
+      })
+      .catch(error => console.error("Get breeds error: ", error));
+  }
+
+  function getDogs() {
+    const url = new URL('https://frontend-take-home-service.fetch.com/dogs/search');
+    if (selectedBreed !== ''){
+      url.searchParams.append('breeds', selectedBreed);
+    }
+    if (zipCodes !== '') {
+      const zipCodesArray = zipCodes.replaceAll(/\s/g, '').split(',')
+
+      zipCodesArray.forEach((zipCode) => {
+        url.searchParams.append('zipCodes', zipCode);
+      })
+    }
+    if (ageMin !== '') {
+      url.searchParams.append('ageMin', ageMin);
+    }
+    if (ageMax !== '') {
+      url.searchParams.append('ageMax', ageMax);
+    }
+    if (sortField !== "") {
+      url.searchParams.append('sort', `${sortField}:${sort}`);
+    }
+    url.searchParams.append('size', size);
+
+    fetch(url, { credentials: 'include' })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Search results: ", data);
+        fetchDogDetails(data.resultIds);
+      })
+      .catch(error => console.error("Search dogs error: ", error));
+  }
+
+  function fetchDogDetails(dogIds) {
+    fetch('https://frontend-take-home-service.fetch.com/dogs', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(dogIds),
+      credentials: 'include',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Dog details: ", data);
+        setDogs(data);
+        // all dogs with their details
+      })
+      .catch(error => console.error("Fetch dog details error: ", error));
+  }
+
+  function getMatch() {
+    fetch('https://frontend-take-home-service.fetch.com/dogs/match', {
+      method: 'POST',
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(selectedDogs.map(dog => dog.id)),
+      credentials: 'include',
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Request failed!');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("Match found: ", data);
+        // data is equal to a dog's id
+        // so we need to get the dog entire object using the dog id and save that to match (instead of the id)
+        const matchingDog = selectedDogs.find((dog) => dog.id === data.match)
+        setMatch(matchingDog);
+        // dogs that match user input
+      })
+      .catch(error => console.error("Get match error: ", error));
+  }
+  
+  console.log(selectedDogs)
+  console.log(match)
+ 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Fetch Dog Finder</h1>
+
+      {
+        isLoggedIn === false
+        &&
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input id="name" type="text" name="name" value={name} onChange={e => setName(e.target.value)} />
+
+          <label htmlFor="email">Email:</label>
+          <input id="email" type="email" name="email" value={email} onChange={e => setEmail(e.target.value)} />
+
+          <button onClick={login}>Login</button>
+        </div>
+      }
+      {
+        isLoggedIn === true 
+        &&
+        <div>
+          <div>
+            <label htmlFor="breed">Breed:</label>
+            <select id="breed" value={selectedBreed} onChange={e => setSelectedBreed(e.target.value)}>
+              <option value="">-- Choose a Breed --</option>
+              {breeds.map(breed => <option key={breed} value={breed}>{breed}</option>)}
+            </select>
+
+            <label htmlFor="zipCodes">Zip Codes:</label>
+            <input id="zipCodes" type="text" name="zipCodes" value={zipCodes} onChange={e => setZipCodes(e.target.value)} />
+
+            <label htmlFor="ageMin">Minimum Age:</label>
+            <input id="ageMin" type="number" name="ageMin" value={ageMin} onChange={e => setAgeMin(e.target.value)} />
+
+            <label htmlFor="ageMax">Maximum Age:</label>
+            <input id="ageMax" type="number" name="ageMax" value={ageMax} onChange={e => setAgeMax(e.target.value)} />
+
+            <label htmlFor="size">Size:</label>
+            <input id="size" type="number" name="size" value={size} onChange={e => setSize(parseInt(e.target.value))} />
+
+            <label htmlFor="sortField">Sort Field:</label>
+            <input id="softField" type="text" name="sortField" value={sortField} onChange={e => setSortField(e.target.value)} />
+
+            <label htmlFor="sort">Sort:</label>
+            <input id="sort" type="text" name="sort" value={sort} onChange={e => setSort(e.target.value)} />
+
+            <button onClick={getDogs}>Search Dogs</button>
+          </div>
+
+          <div className="cardsContainer">
+            {dogs.map(dog => (
+              <div className="card" key={dog.id}>
+                <div className="cardImg">
+                  <img src={dog.img} alt={dog.name} />
+                </div>
+                <p className="cardTitle">{dog.name}</p>
+                <p className="cardContent">Breed: {dog.breed}</p> 
+                <p className="cardContent">Zip Code: {dog.zip_code}</p>
+                <button className="cardBtn" onClick={() => handleSelectDog(dog)}>Select Dog</button>
+              </div>
+            ))}
+          </div>
+
+          {
+            selectedDogs.length > 0 
+            &&
+            <button onClick={getMatch}>Find a Match</button>
+          }
+
+          {match && (
+            <div>
+              <h2>Match Found!</h2>
+              <img src={match.img} alt={match.name} />
+              <p>{match.name}</p>
+            </div>
+          )}
+        </div>
+      }
+    </div>
+  );
 }
 
-export default App
+export default App;
